4895cb0340a8fd52ec43c3f5878c23733814511b
ARNOLD G NUNEZ <31264997+Arnold-Nunez@users.noreply.github.com>
2018-03-04 23:16:58 -0800

    Update app.js

--- Project 6/js/app.js
+++ Project 6/js/app.js
@@ -1,11 +1,11 @@
 // JS
 let video = document.querySelector('video');
-let time = video.currentTime;
 let span = document.getElementsByTagName('span');
 
 video.addEventListener('timeupdate', () => {
+  let time = video.currentTime;
   for (i = 0; i < span.length; i += 1) {
-    if(span[i].getAttribute('data-start') < time < span[i].getAttribute('data-end')) {
+    if(span[i].getAttribute('data-start') < time && time < span[i].getAttribute('data-end')) {
       span[i].style.color= '#eb9373';
     } else {
       span[i].style.color= '#000';
